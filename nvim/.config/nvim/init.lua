1c5c15b3ece812dc9ef43424679adafa46b40a8a
libresoul <contact@damindu.me>
2026-06-15 18:34:28 +0530

    (nvim): switch to 2 space indenting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,8 +2,8 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.cmd('filetype indent on')
@@ -31,19 +31,19 @@ vim.keymap.set("n", "<C-h>", vim.cmd.tabprev)
 vim.keymap.set("n", "<C-l>", vim.cmd.tabnext)
 vim.keymap.set("n", "<leader>g", vim.cmd.Neogit)
 vim.keymap.set("n", "<leader>x", function()
-    vim.cmd("split")
-    vim.cmd("wincmd j")
-    vim.cmd("set nonumber")
-    vim.cmd("set norelativenumber")
-    vim.cmd("resize 12")
-    vim.cmd("terminal")
+  vim.cmd("split")
+  vim.cmd("wincmd j")
+  vim.cmd("set nonumber")
+  vim.cmd("set norelativenumber")
+  vim.cmd("resize 12")
+  vim.cmd("terminal")
 end)
 vim.keymap.set("n", "<leader>z", function()
-    vim.cmd("vsplit")
-    vim.cmd("wincmd l")
-    vim.cmd("set nonumber")
-    vim.cmd("set norelativenumber")
-    vim.cmd("terminal")
+  vim.cmd("vsplit")
+  vim.cmd("wincmd l")
+  vim.cmd("set nonumber")
+  vim.cmd("set norelativenumber")
+  vim.cmd("terminal")
 end)
 
 vim.keymap.set("n", "<leader>y", "\"+y")
@@ -63,24 +63,25 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 -- PACKAGES
 vim.pack.add({
-    { src = "https://github.com/vague-theme/vague.nvim" },
-    { src = "https://github.com/nvim-mini/mini.pick" },
-    { src = "https://github.com/nvim-mini/mini.icons" },
-    { src = "https://github.com/neovim/nvim-lspconfig" },
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-    { src = "https://github.com/L3MON4D3/LuaSnip" },
-    { src = "https://github.com/rafamadriz/friendly-snippets" },
-    { src = "https://github.com/cohama/lexima.vim" },
-    { src = "https://github.com/saghen/blink.cmp" },
-    { src = "https://github.com/lambdalisue/vim-suda" },
-    { src = "https://github.com/nvim-lua/plenary.nvim" },
-    { src = "https://github.com/nvim-flutter/flutter-tools.nvim" },
-    { src = "https://github.com/stevearc/conform.nvim" },
-    { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/ThePrimeagen/harpoon",           version = "harpoon2" },
-    { src = "https://github.com/esmuellert/codediff.nvim" },
-    { src = "https://github.com/NeogitOrg/neogit" },
+  { src = "https://github.com/vague-theme/vague.nvim" },
+  { src = "https://github.com/nvim-mini/mini.pick" },
+  { src = "https://github.com/nvim-mini/mini.icons" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/mason-org/mason.nvim" },
+  { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+  { src = "https://github.com/L3MON4D3/LuaSnip" },
+  { src = "https://github.com/rafamadriz/friendly-snippets" },
+  { src = "https://github.com/cohama/lexima.vim" },
+  { src = "https://github.com/saghen/blink.lib" },
+  { src = "https://github.com/saghen/blink.cmp" },
+  { src = "https://github.com/lambdalisue/vim-suda" },
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/nvim-flutter/flutter-tools.nvim" },
+  { src = "https://github.com/stevearc/conform.nvim" },
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/ThePrimeagen/harpoon",           version = "harpoon2" },
+  { src = "https://github.com/esmuellert/codediff.nvim" },
+  { src = "https://github.com/NeogitOrg/neogit" },
 })
 
 -- MASON
@@ -89,16 +90,16 @@ require("mason").setup()
 
 -- Icons
 require('mini.icons').setup({
-    -- style = 'ascii',
+  -- style = 'ascii',
 })
 
 -- Pick
 
 require('mini.pick').setup({
-    -- mappings = {
-    --     move_down = '<C-j>',
-    --     move_up = '<C-k>',
-    -- },
+  -- mappings = {
+  --     move_down = '<C-j>',
+  --     move_up = '<C-k>',
+  -- },
 })
 
 -- vim.keymap.set('n', '<leader>pf', function()
@@ -118,17 +119,17 @@ vim.keymap.set('n', '<leader>pg', '<cmd>Pick grep_live<CR>')
 
 -- oil
 require("oil").setup({
-    columns = {
-        "icon",
-    },
-    -- Buffer-local options to use for oil buffers
-    buf_options = {
-        buflisted = false,
-        bufhidden = "hide",
-    },
-    view_options = {
-        show_hidden = true,
-    },
+  columns = {
+    "icon",
+  },
+  -- Buffer-local options to use for oil buffers
+  buf_options = {
+    buflisted = false,
+    bufhidden = "hide",
+  },
+  view_options = {
+    show_hidden = true,
+  },
 })
 
 -- flutter tools
@@ -156,17 +157,17 @@ vim.keymap.set("n", "<A-l>", function() harpoon:list():next() end)
 local conform = require("conform")
 
 conform.setup({
-    formatters_by_ft = {
-        python = { "black" },
-        json = { "jq" },
-        javascript = { "biome" },
-        javascriptreact = { "biome" },
-        typescript = { "biome" },
-        typescriptreact = { "biome" },
-    },
-    default_format_opts = {
-        lsp_format = "fallback"
-    }
+  formatters_by_ft = {
+    python = { "black" },
+    json = { "jq" },
+    -- javascript = { "biome" },
+    -- javascriptreact = { "biome" },
+    -- typescript = { "biome" },
+    -- typescriptreact = { "biome" },
+  },
+  default_format_opts = {
+    lsp_format = "fallback"
+  }
 })
 
 vim.keymap.set("n", "<leader>ff", function() conform.format() end)
@@ -176,63 +177,63 @@ vim.keymap.set("v", "<leader>ff", function() conform.format() end)
 require("mason-lspconfig").setup()
 
 vim.lsp.config('lua_ls', {
-    settings = {
-        Lua = {
-            runtime = {
-                version = "LuaJIT",
-            },
-            diagnostics = {
-                globals = {
-                    'vim',
-                    'require'
-                },
-            },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            telemetry = {
-                enable = false,
-            },
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = {
+          'vim',
+          'require'
         },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
     },
+  },
 })
 
 vim.lsp.config('tailwindcss', {
 })
 
 vim.lsp.config('ts_ls', {
-    root_dir = function(bufnr, on_dir)
-        local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
-        root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
-            or vim.list_extend(root_markers, { '.git' })
+  root_dir = function(bufnr, on_dir)
+    local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+    root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
+        or vim.list_extend(root_markers, { '.git' })
 
-        -- Only check for deno.json/deno.jsonc, ignore deno.lock
-        local deno_root = vim.fs.root(bufnr, { 'deno.json', 'deno.jsonc' })
-        local project_root = vim.fs.root(bufnr, root_markers)
+    -- Only check for deno.json/deno.jsonc, ignore deno.lock
+    local deno_root = vim.fs.root(bufnr, { 'deno.json', 'deno.jsonc' })
+    local project_root = vim.fs.root(bufnr, root_markers)
 
-        if deno_root and (not project_root or #deno_root >= #project_root) then
-            return
-        end
+    if deno_root and (not project_root or #deno_root >= #project_root) then
+      return
+    end
 
-        on_dir(project_root or vim.fn.getcwd())
-    end,
+    on_dir(project_root or vim.fn.getcwd())
+  end,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args)
-        local clients = vim.lsp.get_clients({ bufnr = args.buf })
-        local formatters = conform.list_all_formatters()
+  pattern = "*",
+  callback = function(args)
+    local clients = vim.lsp.get_clients({ bufnr = args.buf })
+    local formatters = conform.list_all_formatters()
 
-        if #formatters > 0 then
-            conform.format()
-            return
-        end
+    if #formatters > 0 then
+      conform.format()
+      return
+    end
 
-        if #clients > 0 then
-            vim.lsp.buf.format({ bufnr = args.buf })
-        end
-    end,
+    if #clients > 0 then
+      vim.lsp.buf.format({ bufnr = args.buf })
+    end
+  end,
 }) -- An autocommand that formats on save
 
 -- vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format)
@@ -247,41 +248,41 @@ require("luasnip.loaders.from_vscode").lazy_load()
 -- COMPLETIONS
 
 require("blink.cmp").setup({
-    enabled = function()
-        return vim.bo.filetype ~= 'oil' and vim.bo.buftype ~= 'prompt'
-    end,
-    keymap = {
-        preset = 'super-tab',
-        ['<C-space>'] = { function(cmp) cmp.show({ providers = { 'snippets' } }) end },
-        ['<CR>'] = { 'accept', 'fallback' },
+  enabled = function()
+    return vim.bo.filetype ~= 'oil' and vim.bo.buftype ~= 'prompt'
+  end,
+  keymap = {
+    preset = 'super-tab',
+    ['<C-space>'] = { function(cmp) cmp.show({ providers = { 'snippets' } }) end },
+    ['<CR>'] = { 'accept', 'fallback' },
+  },
+  signature = { enabled = true },
+  completion = {
+    ghost_text = { enabled = true },
+    documentation = { auto_show = true },
+    menu = {
+      draw = {
+        treesitter = { "lsp" }
+      }
     },
-    signature = { enabled = true },
-    completion = {
-        ghost_text = { enabled = true },
-        documentation = { auto_show = true },
-        menu = {
-            draw = {
-                treesitter = { "lsp" }
-            }
-        },
-    },
-    fuzzy = { implementation = "lua" }
+  },
+  fuzzy = { implementation = "lua" }
 })
 
 -- DIAGNOSTICS
 
 vim.diagnostic.config({
-    virtual_text = true,
-    signs = true,
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
+  virtual_text = true,
+  signs = true,
+  update_in_insert = true,
+  underline = true,
+  severity_sort = true,
 })
 
 vim.api.nvim_set_keymap('n', '<A-j>', '<cmd>lua vim.diagnostic.jump({count = 1})<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<A-k>', '<cmd>lua vim.diagnostic.jump({count = -1})<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>e', function()
-    vim.diagnostic.open_float({ scope = 'line' })
+  vim.diagnostic.open_float({ scope = 'line' })
 end)
 
 -- NEOGIT
@@ -289,38 +290,38 @@ end)
 local neogit = require("neogit")
 
 neogit.setup {
-    -- Disable the filewatcher to prevent constant status updates
-    filewatcher = {
-        interval = 1000,
-        enabled = false, -- Set to false to stop monitoring .git/ directory
-    },
+  -- Disable the filewatcher to prevent constant status updates
+  filewatcher = {
+    interval = 1000,
+    enabled = false, -- Set to false to stop monitoring .git/ directory
+  },
 
-    -- Disable auto-refresh to prevent automatic buffer updates
-    auto_refresh = false, -- Manually refresh with your keybinding when needed
+  -- Disable auto-refresh to prevent automatic buffer updates
+  auto_refresh = false, -- Manually refresh with your keybinding when needed
 
-    -- Other relevant settings
-    disable_insert_on_commit = false
+  -- Other relevant settings
+  disable_insert_on_commit = false
 }
 
 -- COLORS
 require("vague").setup({
-    transparent = false,
-    bold = true,
-    italic = false,
-    colors = {
-        bg = "#141415",
-        inactiveBg = "#1c1c24",
-        fg = "#cdcdcd",
-        floatBorder = "#878787",
-        line = "#3e3e52",
-        comment = "#606079",
-        builtin = "#b4d4cf",
-        func = "#c48282",
-        string = "#e8b589",
-        number = "#e0a363",
-        property = "#c3c3d5",
-        constant = "#aeaed1",
-    },
+  transparent = false,
+  bold = true,
+  italic = false,
+  colors = {
+    bg = "#141415",
+    inactiveBg = "#1c1c24",
+    fg = "#cdcdcd",
+    floatBorder = "#878787",
+    line = "#3e3e52",
+    comment = "#606079",
+    builtin = "#b4d4cf",
+    func = "#c48282",
+    string = "#e8b589",
+    number = "#e0a363",
+    property = "#c3c3d5",
+    constant = "#aeaed1",
+  },
 })
 
 vim.cmd("colorscheme vague")
